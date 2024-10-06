51855eca2c5d1bafe7bd3ca0ffab72aede388009
Phil Kitchen <philkit6@gmail.com>
2024-10-06 22:59:12 +0100

    added some stuff for stirypoints. not working may need to add something into packages

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,12 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import React from 'react';
+import ForgeReconciler, { Text, useProductContext } from '@forge/react';
 import { invoke } from '@forge/bridge';
 import { LineChart } from '@forge/react';
+import { requestJira } from '@forge/bridge';
+import { DynamicTable, Link } from "@forge/react";
+
+
+const context = useProductContext();
+
+const [storyPoints, setStoryPoints] = React.useState(null);
+
+const fetchStoryPointsForIssue = async () => {
+  const issueId = context?.extension.issue.id;
+  const res = await requestJira(`/rest/api/3/issue/${issueId}`);
+  const data = await res.json();
+  return data.fields["customfield_10035"]; // Access the storypoints field 
+};
+
+React.useEffect(() => {
+  if (context) {
+    fetchStoryPointsForIssue().then(setStoryPoints);
+  }
+}, [context]);
 
 // Sample data array (can be moved to a separate file if needed)
 const arrayData = [
   // in this example ['x value', 'y value', 'color value']
-  ['Apple', 10, 'Dog'],
+  ['Apple',`${storyPoints}`, 'Dog'],
   ['Apple', 1, 'Cat'],
   ['Apple', 25, 'Horse'],
   ['Apple', 5, 'Elephant'],
